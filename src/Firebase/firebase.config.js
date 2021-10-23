7291dc9eb1d8a7328431adc71c1e8c39c6d796cf
arunabhnewar <newardevil@gmail.com>
2021-10-24 01:00:45 +0600

    img added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDbCbGmiMWOPega3-HTu35Xa7I8rYuKelo",
-    authDomain: "ema-john-simple-react-7205a.firebaseapp.com",
-    projectId: "ema-john-simple-react-7205a",
-    storageBucket: "ema-john-simple-react-7205a.appspot.com",
-    messagingSenderId: "847474229221",
-    appId: "1:847474229221:web:886bbe6d52082cb02d390c"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
